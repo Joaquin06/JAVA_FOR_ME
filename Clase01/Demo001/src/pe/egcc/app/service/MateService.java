a0587c8c482c983bad147c38a2e1a21bb55eb3dc
Joaquin Huauya Arotinco <joaquin.aha@hotmail.com>
2016-04-24 17:41:42 -0500

    Subiendo

---
+++ Clase01/Demo001/src/pe/egcc/app/service/MateService.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.egcc.app.service;
+
+/**
+ *
+ * @author Alumno
+ */
+public class MateService { //servicio de matematicas
+    
+    public int sumar(int n1,int n2){
+    return (n1+n2);
+    
+    }
+    
+}
